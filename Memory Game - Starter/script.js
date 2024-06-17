7249de46aae97300caeb057871b861cfe0f4ed96
Matthew Haynes <124mhaynes@frhsd.com>
2024-06-17 11:24:38 -0400

    tried my best to do the code

--- Memory Game - Starter/script.js
+++ Memory Game - Starter/script.js
@@ -1,24 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     //create an array of card objects that hold a name and the image for each card
-  
+  const cardArray = [ {name: 'fries', img: 'images/fries.png'}, {name: 'cheeseburger', img: 'images/cheeseburger.png' }, {name: 'hotdog', img: 'images/hotdog.png' }, {name: 'ice-cream', img: 'images/ice-cream.png' }, {name: 'milkshake', img: 'images/milkshake.png' }, {name: 'pizza', img: 'images/pizza.png' },{name: 'fries', img: 'images/fries.png'}, {name: 'cheeseburger', img: 'images/cheeseburger.png' }, {name: 'hotdog', img: 'images/hotdog.png' }, {name: 'ice-cream', img: 'images/ice-cream.png' }, {name: 'milkshake', img: 'images/milkshake.png' }, {name: 'pizza', img: 'images/pizza.png' }]
     //this will shuffle the cards at the beginning of the game
     // DON'T CHANGE THE LINE OF CODE BELOW THIS
     cardArray.sort(() => 0.5 - Math.random())
   
     // select the grid from the HTML and save in a variable
-  
+  var cardGrid = document.querySelector(".grid")
     // select the HTML element that shows the results and save in a variable
-  
+  const resultDisplay = document.querySelector('#result')
     // create array variables for the cardsChosen, cardsChosenId, and cardsWon
-  
+  let cardsChosen = []; 
+  let cardsChosenId = [];
+  let cardsWon = [];
     //create your board
     function createBoard() {
-      // create a for loop that:
+      // create a for loop that
       // 1. creates an img element and saves it in a variable called card
       // 2. sets the card's src to the blank image
       // 3. sets an attribute of the card called 'data-id' to a unique number
       // 4. adds an event listener to the card
       // 5. appends the card to the grid
+      for(let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        cardGrid.appendChild(card)
+      }
     }
   
     //check for matches
@@ -28,7 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
       // to the data-id of the first card that was clicked
       // 3. create a variable called optionTwoId and set it equal
       // to the data-id of the second card that was clicked
-  
+  const cards = document.querySelectorAll('img');
+  var optionOneId = cardsChosen[0];
+  var optionTwoId = cardsChosen[1];
+
       // create an IF statement that checks the following:
       //
       // 1. is the first card the same card as the second card picked? ex. they clicked the same card twice
@@ -47,7 +59,15 @@ document.addEventListener('DOMContentLoaded', () => {
       // --- a. set the image of two cards chosen to blank.png
       // --- b. create an alert function that says "Sorry, try again"
       // END OF IF STATMENT
-  
+  if (optionOneId === optionTwoId){
+    cardsChosen[0].setAttribute('src', 'images/blank.png'),
+    cardsChosen[1].setAttribute('src', 'images/blank.png')
+  }
+  else if(cardsChosen[0] === cardsChosen[1]){
+    cardsChosen[0].setAttribute('src', 'images/white.png'),
+    cardsChosen[1].setAttribute('src', 'images/white.png')
+
+  }
   
       // reset the chosen cards by setting the variable equal to an empty array
       // reset the chosen ids by setting the variable equal to an empty array
